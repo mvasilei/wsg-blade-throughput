bb06510a6309046dff851df87f8c7023295757de
mvasilei <53435274+mvasilei@users.noreply.github.com>
2022-03-30 10:38:37 +0100

    Update wsg-thruput.py

--- wsg-thruput.py
+++ wsg-thruput.py
@@ -1,5 +1,4 @@
-#! /usr/bin/env python
-
+#! /usr/bin/env python2.7
 '''
    This script calculates the throughput per tunnel on a SAMI blade
    "Usage: ./wsg-thruput.py XRM_CA_02_WSG01"
@@ -10,7 +9,11 @@
 
 
 '''
-import getpass, paramiko, sys, re, time
+import getpass, paramiko, sys, re, time, signal
+
+def signal_handler(sig, frame):
+    print('Exiting gracefully Ctrl-C detected...')
+    sys.exit()
 
 def progress(count, total, status=''):
     bar_len = 60
@@ -22,27 +25,54 @@ def progress(count, total, status=''):
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
     sys.stdout.flush()
 
-def connection_establishment():
-   try:
-      client = paramiko.SSHClient()
-      client.load_system_host_keys()
-      client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-      client.connect(SECGW, 22, username=USER, password=PASS)
-      channel = client.invoke_shell()
-      out = channel.recv(65535)
-   except paramiko.AuthenticationException as error:
-      print 'Authentication Error'
-      exit()
-   return (channel,client)
+def connection_establishment(USER, PASS, host):
+    try:
+        print('Processing HOST: ' + host)
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(host, 22, username=USER, password=PASS)
+        channel = client.invoke_shell()
+        while not channel.recv_ready():
+            time.sleep(0.5)
+
+        output = channel.recv(8192)
+    except paramiko.AuthenticationException as error:
+        print ('Authentication Error on host: ' + host)
+        exit()
+    except IOError as error:
+        print (error)
+        exit()
+
+    return (channel, client)
 
 def connection_teardown(client):
    client.close()
 
-def execute_command(command, channel,wait):
-   channel.send(command)
-   time.sleep(wait)
-   out = channel.recv(65535)
-   return (out)
+def execute_command(command, channel):
+    cbuffer = []
+    data = ''
+
+    channel.send(command)
+    while True:
+        if channel.recv_ready():
+            data = channel.recv(1000)
+            cbuffer.append(data)
+
+        time.sleep(0.02)
+        data_no_trails = data.strip()
+
+        if len(data_no_trails) > 0: #and
+            if (data_no_trails[-1] == '#'):
+                break
+
+    if channel.recv_ready():
+        data = channel.recv(1000)
+        cbuffer.append(data)
+
+    rbuffer = ''.join(cbuffer)
+    return (rbuffer)
+
 
 def peer_list(IP):
     peer = []
@@ -51,13 +81,13 @@ def peer_list(IP):
 
     return peer
 
-def bytes_value(peer):
+def bytes_value(peer, channel):
     Bytes = []
     elements = []
 
     crypto = 'show crypto ipsec sa remote-ip' + peer + ' | be Bytes | i "Decrypted|Encrypted"\n'
 
-    out = execute_command(crypto, channel, 2)
+    out = execute_command(crypto, channel)
     raw = out.split('\r\n')
 
     for j in range(1, 3):
@@ -67,51 +97,54 @@ def bytes_value(peer):
 
     return Bytes
 
-def bytes_encr_decr(IP, peer):
-    pastbytes = bytes_value(peer)
+def bytes_encr_decr(IP, peer, channel):
+    pastbytes = bytes_value(peer, channel)
     print pastbytes
     time.sleep(5)
-    currentbytes = bytes_value(peer)
+    currentbytes = bytes_value(peer, channel)
     print currentbytes
 
     print "\r\nDecrypted throughput for IP", peer, ":", round(((int(currentbytes[0][0]) - int(pastbytes[0][0])) * 8) * 0.000001 / 5, 2), "Mbps"
     print "Encrypted throughput for IP", peer, ":", round(((int(currentbytes[0][1]) - int(pastbytes[0][1])) * 8) * 0.000001 / 5, 2), "Mbps"
 
-if __name__ == '__main__':
-USER = 'USERNAME'#raw_input('Username: ')
-PASS = 'PASSWORD'#getpass.getpass(prompt='Password: ')
+def main():
+    USER = raw_input('Username: ')
+    PASS = getpass.getpass(prompt='Password: ')
 
-if len (sys.argv) != 2 :
-    print "Usage: ./wsg-thruput.py XRM_CA_02_WSG01"
-    sys.exit (1)
+    if len (sys.argv) != 2 :
+        print "Usage: /wsg_throughput.py XRM_CA_02_WSG01"
+        sys.exit (1)
 
-host = sys.argv[1]
-p = re.compile(r'X[A-Z]{2}_CA_0[12]_WSG[0-1][1-9]')
-if (not re.findall(p,sys.argv[1])):
-    print "Check your hostname"
-    sys.exit (1)
+    host = sys.argv[1]
+    p = re.compile(r'X[A-Z]{2}_CA_0[12]_WSG[0-1][1-9]')
+    if (not re.findall(p,sys.argv[1])):
+        print "Check your hostname"
+        sys.exit (1)
 
-SAMI = host[-2:]
-SECGW = host.replace('_', '-')[0:9]
+    SAMI = host[-2:]
+    SECGW = host.replace('_', '-')[0:9]
 
-channel, client = connection_establishment()
+    channel, client = connection_establishment(USER, PASS, SECGW)
 
-execute_command('session slot ' + SAMI + ' process 3\n', channel, 10)
-print 'Connected to', host
-out = execute_command('sho crypto ha info\n', channel, 2)
+    execute_command('session slot ' + SAMI + ' process 3\n', channel)
+    print 'Connected to', host
+    out = execute_command('sho crypto ha info\n', channel)
 
-if not re.findall('ACTIVE', out):
-    print "Specified SAMI isn't active on this device"
+    if not re.findall('ACTIVE', out):
+        print "Specified SAMI isn't active on this device"
+        connection_teardown(client)
+        sys.exit(1)
+
+    execute_command('term len 0\n', channel)
+    RemoteIP = execute_command('sho crypto isakmp sa | i "Remote IP"\n', channel)
+    IP = RemoteIP.split('\r\n')
+
+    peer = peer_list(IP)
+    for i in range(0, len(peer)):
+        bytes_encr_decr(IP, peer[i], channel)
+
     connection_teardown(client)
-    sys.exit(1)
 
-execute_command('term len 0\n', channel, 1)
-RemoteIP = execute_command('sho crypto isakmp sa | i "Remote IP"\n', channel, 5)
-IP = RemoteIP.split('\r\n')
-
-peer = peer_list(IP)
-for i in range(0, len(peer)):
-    bytes_encr_decr(IP, peer[i])
-
-
-connection_teardown(client)
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)  #catch ctrl-c and call handler to terminate the script
+    main()
